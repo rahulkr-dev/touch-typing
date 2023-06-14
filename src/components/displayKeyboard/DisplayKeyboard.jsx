85336c0337df64939adc720c03b463a3ea0eeb54
rahul kumar <rkrahul8181@gmail.com>
2023-06-14 14:03:26 +0530

    Done visually able to see next character

--- src/components/displayKeyboard/DisplayKeyboard.jsx
+++ src/components/displayKeyboard/DisplayKeyboard.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import Keys from './Keys';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Keys from "./Keys";
+import { initialNextChar } from "../../app/typingSlice";
 const DisplayKeyboard = () => {
-    const {displayKeys} = useSelector(state=>state.touchTyping);
-
+  const { displayKeys } = useSelector((state) => state.touchTyping);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(initialNextChar());
+  }, []);
   return (
     <div>
-        {displayKeys.map((obj)=>(
-            <Keys key={obj.value} value={obj.value} />
-        ))}
+      {displayKeys.map((obj) => (
+        <Keys key={obj.value} value={obj.value} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default DisplayKeyboard
+export default DisplayKeyboard;
--- src/components/displayKeyboard/Keys.jsx
+++ src/components/displayKeyboard/Keys.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
 const Keys = ({ value }) => {
-  return <button>{value}</button>;
+    const {nextChar} = useSelector(state=>state.touchTyping);
+    console.log(nextChar)
+    const flag = nextChar==value
+  return <button className={`button ${flag?"active":"not-active"}`}>{value}</button>;
 };
 
 export default Keys;

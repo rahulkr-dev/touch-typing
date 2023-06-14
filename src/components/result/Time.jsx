e81d2c35798870140c01f746efcc3b851bca98c0
rahul kumar <rkrahul8181@gmail.com>
2023-06-14 17:19:55 +0530

    Add startTest and End test

--- src/components/result/Time.jsx
+++ src/components/result/Time.jsx
@@ -3,6 +3,7 @@ import style from "./result.module.css";
 const Time = () => {
   const [time, setTime] = useState(300); // 5 minutes in seconds
   const [isRunning, setIsRunning] = useState(false);
+  const [testStart,setTestStart] = useState(false)
   const ref = useRef(null)
 
   useEffect(() => {
@@ -31,15 +32,26 @@ const Time = () => {
     setIsRunning(true);
   };
 
+  const handleStartTest = ()=>{
+    setTestStart(true)
+    handleStartTimer()
+  }
+
   const handleResetTimer = () => {
     setIsRunning(false);
     setTime(300);
   };
+
+  const handleEndTest = () =>{
+    setTestStart(false)
+    handleResetTimer()
+  }
   return (
     <div className={style.box}>
       <div>{formatTime(time)}</div>
       <div>Time</div>
-      <div onClick={()=>handleStartTimer()} className={style.startTest}>Start Test</div>
+      <div onClick={handleStartTest} className={`${style.startTest} ${testStart?"displayNone":"displayBlock"}`}>Start Test</div>
+      <div onClick={handleEndTest} className={`${style.startTest} ${testStart?"displayBlock":"displayNone"}`}>End Test</div>
     </div>
   );
 };
